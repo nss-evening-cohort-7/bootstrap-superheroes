217349d80656017f467d190f63d9c6bb58349622
Anna Dolan <anna.dolan115@gmail.com>
2018-04-02 21:22:40 -0500

    added superhero pics and names

--- javascripts/superheroes.js
+++ javascripts/superheroes.js
@@ -1,1 +1,36 @@
-console.log("superheroes");
+const printToDom = (string, id) => {
+    document.getElementById(id).innerHTML = string;
+}
+
+const buildDomString = (array) => {
+    let domString = "";
+    array.forEach((superhero)=>{
+        domString += `<div>`;
+        domString += `<img src="${superhero.image}">`;
+        domString += `<h2>${superhero.name}</h2>`;
+        domString += `<div>`;
+        domString += `<div>`;
+        domString += `<div>`;
+    })
+    printToDom(domString, "superheroes-container");
+}
+
+
+function executeThisFunctionAfterFileLoads(){
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.superheroes);
+  }
+
+  function WTF(){
+    console.log("something went wrong");
+  }
+  
+  const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", executeThisFunctionAfterFileLoads);
+    myRequest.addEventListener("error", WTF);
+    myRequest.open("GET", "../db/superheroes.json");
+    myRequest.send();
+  };
+
+  startApplication();
